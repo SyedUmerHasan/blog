88da5c5fe7c09645c6ad37c6d9bd238143e6d9c7
umerhasan2010 <umerhasan2010@outlook.com>
2020-05-08 18:18:37 +0500

    adding project

--- config/services.php
+++ config/services.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | as Mailgun, SparkPost and others. This file provides a sane default
     | location for this type of information, allowing packages to have
     | a conventional file to locate the various service credentials.
     |
@@ -29,5 +29,13 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+    // We need to provide default values to validate types
+    'GOOGLE_APPLICATION_CREDENTIALS' => [
+        'GOOGLE_APPLICATION_CREDENTIALS' => env('GOOGLE_APPLICATION_CREDENTIALS')
+    ]
 
 ];
